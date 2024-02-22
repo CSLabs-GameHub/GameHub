b7c649fca1ff943357ace4b0123a8be52510f9ce
seantokuzo <seantokuzo@gmail.com>
2024-02-21 18:40:50 -0800

    Updates

--- common/middlewares/current-user.ts
+++ common/middlewares/current-user.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express"
-import jwt from "jsonwebtoken"
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
 
 interface UserPayload {
   id: string
@@ -14,20 +14,17 @@ declare global {
   }
 }
 
-export const currentUser = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export interface CurrentUserRequest extends Request {
+  currentUser?: UserPayload
+}
+
+export const currentUser = (req: Request, res: Response, next: NextFunction) => {
   if (!req.session?.jwt) {
     return next()
   }
 
   try {
-    const payload = jwt.verify(
-      req.session.jwt,
-      process.env.JWT_KEY!
-    ) as UserPayload
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload
     req.currentUser = payload
   } catch (err) {}
 
